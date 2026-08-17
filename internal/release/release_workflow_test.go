879eb10e369d37e9af4267b6c0aaef0654add8e9
yshanchui <qizhidexiaomimei@gmail.com>
2026-08-17 14:01:07 +0800

    fix(release): decouple component versions

--- internal/release/release_workflow_test.go
+++ internal/release/release_workflow_test.go
@@ -13,7 +13,8 @@ func TestReleaseWorkflowUsesPinnedReleaseInputs(t *testing.T) {
 
 	required := []string{
 		"contracts_ref:",
-		"CONTRACTS_REF: ${{ inputs.contracts_ref || github.ref_name }}",
+		"WEB_REF: ${{ inputs.web_ref || 'v0.1.6' }}",
+		"CONTRACTS_REF: ${{ inputs.contracts_ref || 'v0.1.13' }}",
 		"WEB_SHA256: ${{ inputs.web_sha256 }}",
 		"go get \"github.com/opensoha/soha-contracts@${CONTRACTS_REF}\"",
 		"go list -m github.com/opensoha/soha-contracts",
